b0fff7d7f5817c08c92dca64a3fb6a9ddc210883
Stavros Tsikinas <s.tsikinas@epsilonnet.gr>
2022-11-28 12:34:22 +0200

    bind views

--- app/src/main/java/com/example/nlquiz/MainActivity.kt
+++ app/src/main/java/com/example/nlquiz/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.nlquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatEditText
+import com.example.nlquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnLogin: Button
+    private lateinit var etName: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Bind view to replace findViewById
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        btnLogin = binding.btnLogin
+        etName = binding.etName
+
+        btnLogin.setOnClickListener {
+            if (etName.text.isNullOrEmpty())
+                Toast.makeText(this, getString(R.string.empty_name_warning), Toast.LENGTH_LONG).show()
+        }
     }
 }
